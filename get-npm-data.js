e42f38c5cbc16949a67af87956673c4a52600147
Patrick Sullivan <sully317@gmail.com>
2022-07-25 19:45:19 -0400

    fully qualified the descriptions to fix the broken overflow

--- get-npm-data.js
+++ get-npm-data.js
@@ -1,9 +1,9 @@
-const fs = require('fs')
-const axios = require('axios')
-const _ = require('lodash')
-const search = require('./search')
+const fs = require("fs");
+const axios = require("axios");
+const _ = require("lodash");
+const search = require("./search");
 
-const CACHED_DATA_NAME = 'hits.json'
+const CACHED_DATA_NAME = "hits.json";
 
 module.exports = async () => {
   /*
@@ -11,45 +11,51 @@ module.exports = async () => {
    * Comment out if you want to make changes to the data model, e.g. add a new field
    */
   if (fs.existsSync(CACHED_DATA_NAME)) {
-    return fs.promises.readFile(CACHED_DATA_NAME).then(file => JSON.parse(file))
+    return fs.promises
+      .readFile(CACHED_DATA_NAME)
+      .then((file) => JSON.parse(file));
   }
 
-  const keywords = ['gatsby', 'gatsby-plugin']
+  const keywords = ["gatsby", "gatsby-plugin"];
 
-  const buildFilter = keywords.map(keyword => `keywords:${keyword}`)
+  const buildFilter = keywords.map((keyword) => `keywords:${keyword}`);
 
   const hits = await search({
     filters: `(${buildFilter.join(` OR `)})`,
     hitsPerPage: 1000,
-  })
+  });
 
-  let total = hits.length
+  let total = hits.length;
 
-  let enriched_hits = []
+  let enriched_hits = [];
 
   for (const batch of _.chunk(hits, 25)) {
     enriched_hits = enriched_hits.concat(
       await Promise.all(
-        batch.map(pkg => {
+        batch.map((pkg) => {
           return axios(`https://registry.npmjs.org/${pkg.name}/${pkg.version}`)
-            .then(res => {
+            .then((res) => {
               return Object.assign({}, res.data, {
-                description: pkg.description
-                  .replace(/,/g, '","')
-                  .replace(/\n/g, ' '),
+                description: `"${pkg.description
+                  // .replace(/,/g, '","')
+                  .replace(/\n/g, " ")}"`,
                 downloads: pkg.downloadsLast30Days,
-                lastUpdated: new Date(pkg.modified)
-              })
+                lastUpdated: new Date(pkg.modified),
+              });
             })
-            .catch(() => null)
+            .catch(() => null);
         })
-      ).then(all => all.filter(Boolean))
-    )
+      ).then((all) => all.filter(Boolean))
+    );
 
-    console.log(`progress: ${enriched_hits.length} of ${total}`)
+    console.log(`progress: ${enriched_hits.length} of ${total}`);
   }
 
-  await fs.promises.writeFile('hits.json', JSON.stringify(enriched_hits, null, 2), 'utf8')
+  await fs.promises.writeFile(
+    "hits.json",
+    JSON.stringify(enriched_hits, null, 2),
+    "utf8"
+  );
 
-  return enriched_hits
-}
+  return enriched_hits;
+};
