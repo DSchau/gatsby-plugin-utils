b621819a9cf759ece6a69ea95f285e1c9874a6a1
pragmaticpat <72818141+pragmaticpat@users.noreply.github.com>
2022-07-25 21:23:50 -0400

    fully qualified the descriptions to fix the broken overflow (#1)

--- index.js
+++ index.js
@@ -1,65 +1,71 @@
-const fs = require('fs')
-const getNpmData = require('./get-npm-data')
-const semver = require('semver')
-const toCSV = require('./to-csv')
+const fs = require("fs");
+const getNpmData = require("./get-npm-data");
+const semver = require("semver");
+const toCSV = require("./to-csv");
 
 async function main() {
-  const hits = await getNpmData()
+  const hits = await getNpmData();
 
-  const packageVersions = hits.reduce((merged, hit) => {
-    try {
-      // order matters here
-      // peerDependencies will "win out", then dependencies, then devDependencies
-      const deps = Object.assign({}, hit.devDependencies, hit.dependencies, hit.peerDependencies)
-      const dep = deps.gatsby
-      const ranges = [
-        ['1.9.9', '1'],
-        ['2.9.9', '2'],
-        [ '3.9.9', '3'],
-        ['4.9.9', '4'],
-      ]
+  const packageVersions = hits.reduce(
+    (merged, hit) => {
+      try {
+        // order matters here
+        // peerDependencies will "win out", then dependencies, then devDependencies
+        const deps = Object.assign(
+          {},
+          hit.devDependencies,
+          hit.dependencies,
+          hit.peerDependencies
+        );
+        const dep = deps.gatsby;
+        const ranges = [
+          ["1.9.9", "1"],
+          ["2.9.9", "2"],
+          ["3.9.9", "3"],
+          ["4.9.9", "4"],
+        ].reverse();
 
-      const pkg = {
-        name: hit.name,
-        description: hit.description,
-        version: hit.version,
-        repository: hit.repository ? hit.repository.url : '',
-        downloads: hit.downloads,
-        lastUpdated: hit.lastUpdated,
-        dependencies: hit.dependencies,
-        devDependencies: hit.devDependencies || {},
-      }
+        const pkg = {
+          name: hit.name,
+          description: hit.description,
+          version: hit.version,
+          repository: hit.repository ? hit.repository.url : "",
+          downloads: hit.downloads,
+          lastUpdated: hit.lastUpdated,
+          dependencies: hit.dependencies,
+          devDependencies: hit.devDependencies || {},
+        };
 
-      let added = false
-      for (const [version, versionName] of ranges) {
-
-        if (semver.satisfies(version, dep)) {
-          merged[versionName].push(pkg)
-          added = true
+        let added = false;
+        for (const [version, versionName] of ranges) {
+          if (semver.satisfies(version, dep)) {
+            merged[versionName].push(pkg);
+            added = true;
+          }
         }
-      }
 
-      if (!added) {
-        merged.unknown.push(pkg)
-      }
+        if (!added) {
+          merged.unknown.push(pkg);
+        }
 
-      return merged
-    } catch (e) {
-      console.error(e)
-      return merged
+        return merged;
+      } catch (e) {
+        console.error(e);
+        return merged;
+      }
+    },
+    {
+      1: [],
+      2: [],
+      3: [],
+      4: [],
+      unknown: [],
     }
-  }, {
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    'unknown': []
-  })
+  );
 
-  await fs.promises.writeFile('versions.csv', toCSV(packageVersions), 'utf8')
+  await fs.promises.writeFile("versions.csv", toCSV(packageVersions), "utf8");
 }
 
-main()
-  .then(() => {
-    console.log('done')
-  })
+main().then(() => {
+  console.log("done");
+});
